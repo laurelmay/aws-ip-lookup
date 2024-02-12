cbf76317eec55094874082e9b7bc770c28954aab
Kyle Laker <kyle@laker.email>
2024-02-12 08:08:16 -0500

    chore: refactor app.js for readability

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 import { isInSubnet } from "https://cdn.jsdelivr.net/npm/is-in-subnet@4.0.1/+esm";
-const ipDataUrl = new URL("https://ip-ranges.amazonaws.com/ip-ranges.json");
-const ipDataResponse = await fetch(ipDataUrl);
-const ipData = await ipDataResponse.json();
 
 function createCell(text) {
   const cell = document.createElement("td");
@@ -12,46 +9,49 @@ function createCell(text) {
   }
   return cell;
 }
-function createRow(address, prefix, regions, services, borderGroups) {
+
+function createRow(match) {
   const row = document.createElement("tr");
-  row.appendChild(createCell(address));
-  row.appendChild(createCell(prefix));
-  row.appendChild(createCell(regions));
-  row.appendChild(createCell(services));
-  row.appendChild(createCell(borderGroups));
+  row.appendChild(createCell(match.ipAddress));
+  row.appendChild(createCell(match.ip_prefix ?? match.ipv6_prefix));
+  row.appendChild(createCell(match.region));
+  row.appendChild(createCell(match.service));
+  row.appendChild(createCell(match.network_border_group));
   return row;
 }
 
-async function getIpv4ForHostname(name) {
-  const response = await fetch('https://cloudflare-dns.com/dns-query?' + new URLSearchParams({type: 'A', name }), {
+async function lookupDnsForHostname(name, type) {
+  const typeNumber = type === 'A' ? 1 : 28;
+  const response = await fetch('https://cloudflare-dns.com/dns-query?' + new URLSearchParams({ type, name }), {
     method: 'GET',
     headers: {
       'Accept': 'application/dns-json',
     },
   });
   const json = await response.json();
-  const answers = json.Answer;
-  return answers?.filter(({ type }) => type === 1).map(({ data }) => data);
+  return json.Answer?.filter((answer) => answer.type === typeNumber).map(({ data }) => data);
 }
 
-async function getIpv6ForHostname(name) {
-  const response = await fetch('https://cloudflare-dns.com/dns-query?' + new URLSearchParams({type: 'AAAA', name }), {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/dns-json',
-    },
-  });
-  const json = await response.json();
-  const answers = json.Answer;
-  return answers?.filter(({ type }) => type === 28).map(({ data }) => data) ?? [];
+function getMatchesHelper(key, objField, ipAddress) {
+  const matches = ipData[key]
+    .filter(({ [objField]: prefix }) => isInSubnet(ipAddress, prefix))
+    .map((match) => ({ ...match, ipAddress }));
+  if (!matches.length) {
+    return [];
+  }
+  if (matches.length == 1) {
+    return matches;
+  }
+  // Remove duplicate entries listed as just being 'AMAZON'
+  return matches.filter(({ service }) => service !== 'AMAZON');
 }
 
 function getMatchesv4(ipAddress) {
-  return ipData["prefixes"].filter(({ ip_prefix }) => isInSubnet(ipAddress, ip_prefix)).map((match) => ({ ...match, ipAddress }));
+  return getMatchesHelper('prefixes', 'ip_prefix', ipAddress);
 }
 
 function getMatchesv6(ipAddress) {
-  return ipData["ipv6_prefixes"].filter(({ ipv6_prefix }) => isInSubnet(ipAddress, ipv6_prefix)).map((match) => ({ ...match, ipAddress }));
+  return getMatchesHelper('ipv6_prefixes', 'ipv6_prefix', ipAddress);
 }
 
 async function handleLookup() {
@@ -65,11 +65,11 @@ async function handleLookup() {
   } else if (ipv6ishRegex.test(text)) {
     matches.push(...getMatchesv6(text));
   } else {
-    const v4Promise = getIpv4ForHostname(text);
-    const v6Promise = getIpv6ForHostname(text);
+    const v4Promise = lookupDnsForHostname(text, 'A');
+    const v6Promise = lookupDnsForHostname(text, 'AAAA');
     const [v4s, v6s] = await Promise.all([v4Promise, v6Promise]);
-    matches.push(...v4s.flatMap((address) => getMatchesv4(address)));
-    matches.push(...v6s.flatMap((address) => getMatchesv6(address)));
+    matches.push(...(v4s?.flatMap((address) => getMatchesv4(address)) ?? []));
+    matches.push(...(v6s?.flatMap((address) => getMatchesv6(address)) ?? []));
   }
   return matches;
 }
@@ -86,14 +86,14 @@ function handleSubmit() {
   handleLookup()
     .then((matches) => {
       if (matches?.length) {
-        const rows = matches.map((match) => createRow(match.ipAddress, match.ip_prefix ?? match.ipv6_prefix, match.region, match.service, match.network_border_group));
+        const rows = matches.map((match) => createRow(match));
         const tableBody = document.getElementById("table-body");
         tableBody.replaceChildren(...rows);
         loading.style.display = "none";
         table.style.display = "table";
       } else {
         const input = document.getElementById("input").value;
-        notFound.innerText = `It looks like ${input} may not be in AWS.`;
+        notFound.innerText = `It looks like '${input}' may not be in AWS.`;
         loading.style.display = "none";
         notFound.style.display = "block";
       }
@@ -102,8 +102,11 @@ function handleSubmit() {
       errorContainer.innerText = "Unable to load data\n" + error + "\n" + error.stack;
       loading.style.display = "none";
       errorContainer.style.display = "block";
-    })
+    });
 }
 
+const ipDataUrl = new URL("https://ip-ranges.amazonaws.com/ip-ranges.json");
+const ipDataResponse = await fetch(ipDataUrl);
+const ipData = await ipDataResponse.json();
 document.getElementById("form").onsubmit = () => handleSubmit();
 
