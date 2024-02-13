300cb4ee201dd4f1c643eee0e0f94405d9d99cdf
Kyle Laker <kyle@laker.email>
2024-02-12 22:54:41 -0500

    feat: add examples to placeholder text

---
+++ populate-placeholder.js
@@ -0,0 +1,25 @@
+const placeholders = [
+  "aws.amazon.com",
+  "canvas.jmu.edu",
+  "checkip.amazonaws.com",
+  "courtlistener.com",
+  "gsa.gov",
+  "ip-ranges.amazonaws.com",
+  "kylelaker.com",
+  "microcenter.com",
+  "mozilla.org",
+  "netflix.com",
+  "pbs.org",
+  "public.cyber.mil",
+  "w3schools.com",
+  "ynab.com",
+  "pokemon.com",
+  "54.245.168.10",
+  "2406:da13::0aef",
+  "2406:da68:800:c100::0001",
+  "15.188.210.64",
+  "3.146.42.64",
+  "3.101.202.127",
+];
+
+document.getElementById("lookup").placeholder = placeholders[Math.floor(Math.random() * placeholders.length)];
