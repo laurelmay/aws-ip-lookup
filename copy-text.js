b1987ddfa5ef3d1d92331bd368647e6f8aff6a4b
Laurel May <laurel@laurelmay.me>
2024-12-31 22:00:39 -0500

    chire: tweak icons slightly

--- copy-text.js
+++ copy-text.js
@@ -23,11 +23,11 @@ export function copyText(text, button, icon) {
   navigator.clipboard.writeText(text)
     .then(() => {
       temporarilyApplyClass(button, 'copy-success');
-      temporarilySwapIcon(icon, { from: 'bi-copy', to: 'bi-check' });
+      temporarilySwapIcon(icon, { from: 'bi-copy', to: 'bi-clipboard-check-fill' });
     })
     .catch(() => {
       temporarilyApplyClass(button, 'copy-failure');
-      temporarilySwapIcon(icon, { from: 'bi-copy', to: 'bi-x' });
+      temporarilySwapIcon(icon, { from: 'bi-copy', to: 'bi-clipboard-x-fill' });
       const textNode = button.parentElement.querySelector('span');
       const selection = window.getSelection();
       const range = document.createRange();
