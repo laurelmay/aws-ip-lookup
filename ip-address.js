d3b3390a4bd27f88cb48047494d7516d3fa5dd95
Laurel May <laurel@laurelmay.me>
2025-05-20 06:54:12 -0400

    feat: allow looking up CIDRs

--- ip-address.js
+++ ip-address.js
@@ -4,13 +4,14 @@ export const IpVersion = Object.freeze({
 });
 
 const v4OctetRegex = '(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])';
-const v4AddressRegex = new RegExp(`^(?:${v4OctetRegex}\\.){3,3}${v4OctetRegex}$`);
+const v4AddressRegex = `(?:${v4OctetRegex}\\.){3,3}${v4OctetRegex}`;
+const v4CidrRegex = new RegExp(`^(?:${v4AddressRegex})(?:\\/(?:[0-9]|[1-2][0-9]|3[0-2]))?$`);
 const v6SegmentRegex = '[0-9a-fA-F]{1,4}';
 // While there are certain IPv6 address constructs outside this regex
 // (specifically link-local and addresses that contain v4 addresses),
 // those are unlikely to be relevant in the case of looking up AWS-hosted
 // IP addresses.
-const v6AddressRegex = new RegExp('^(' +
+const v6AddressRegex = ('(' +
   `(?:${v6SegmentRegex}:){7,7}${v6SegmentRegex}|` +
   `(?:${v6SegmentRegex}:){1,7}:|` +
   `(?:${v6SegmentRegex}:){1,6}:${v6SegmentRegex}|` +
@@ -18,10 +19,11 @@ const v6AddressRegex = new RegExp('^(' +
   `(?:${v6SegmentRegex}:){1,4}(?::${v6SegmentRegex}){1,3}|` +
   `(?:${v6SegmentRegex}:){1,3}(?::${v6SegmentRegex}){1,5}|` +
   `${v6SegmentRegex}:(?::${v6SegmentRegex}){1,6}` +
-  ')$');
+  ')');
+const v6CidrRegex = new RegExp(`^(?:${v6AddressRegex})(?:\\/(?:[0-9]|[1-9][0-9]|1[0-1][1-9]|12[0-8]))?$`);
 
 export function isIpv4Address(address) {
-  return v4AddressRegex.test(address);
+  return v4CidrRegex.test(address);
 }
 
 function ipV4AddressToNumber(address) {
@@ -30,7 +32,7 @@ function ipV4AddressToNumber(address) {
 }
 
 export function isIpv6Address(address) {
-  return v6AddressRegex.test(address);
+  return v6CidrRegex.test(address);
 }
 
 function ipV6AddressToNumber(address) {
@@ -55,25 +57,16 @@ function ipV6AddressToNumber(address) {
 function parseCidr(version, cidr) {
   const [address, mask] = cidr.split('/');
   if (version === IpVersion.IPV4) {
-    return { address: ipV4AddressToNumber(address), mask: parseInt(mask, 10) };
+    if (!isIpv4Address(address)) {
+      throw new Error(`Invalid IPv4 Address: ${address}`);
+    }
+    return { address: ipV4AddressToNumber(address), mask: parseInt(mask ?? 32, 10) };
   }
   if (version === IpVersion.IPV6) {
-    return { address: ipV6AddressToNumber(address), mask: parseInt(mask, 10) };
-  }
-}
-
-function parseAddress(version, ipAddress) {
-  if (version === IpVersion.IPV4) {
-    if (!isIpv4Address(ipAddress)) {
-      throw new Error(`Invalid IPv4 Address: ${ipAddress}`);
+    if (!isIpv6Address(address)) {
+      throw new Error(`Invalid IPv6 Address: ${address}`);
     }
-    return { address: ipV4AddressToNumber(ipAddress), mask: 32 };
-  }
-  if (version === IpVersion.IPV6) {
-    if (!isIpv6Address(ipAddress)) {
-      throw new Error(`Invalid IPv6 Address: ${ipAddress}`);
-    }
-    return { address: ipV6AddressToNumber(ipAddress), mask: 128 };
+    return { address: ipV6AddressToNumber(address), mask: parseInt(mask ?? 128, 10) };
   }
 }
 
@@ -112,7 +105,7 @@ export class CidrTrie {
   }
 
   lookup(ipAddress) {
-    const result = this.#query(parseAddress(this.version, ipAddress));
+    const result = this.#query(parseCidr(this.version, ipAddress));
     if (!result.isPresent) {
       return [];
     }
